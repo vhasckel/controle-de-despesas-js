cf002ce056fccc3ebb3a7816a8b39c5384cf050a
vanessa <cristina202014@outlook.com>
2023-06-28 21:53:15 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,118 @@
+const transactionUl = document.querySelector("#transactions");
+const incomeDisplay = document.querySelector("#money-plus");
+const expenseDisplay = document.querySelector("#money-minus");
+const balanceDisplay = document.querySelector("#balance");
+const form = document.querySelector("#form");
+const inputTransactionName = document.querySelector("#text");
+const inputTransactionAmount = document.querySelector("#amount");
+
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem("transactions")
+);
+
+let transactions =
+  localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
+
+const removeTransaction = (ID) => {
+  transactions = transactions.filter((transaction) => transaction.id !== ID);
+  updateLocalStorage();
+  init();
+};
+
+const addTransactionsIntoDOM = ({ amount, name, id }) => {
+  //se 'amount' for menor que 'transaction', 'operator' irá guardar o valor '-', senão '+'.
+  const operator = amount < 0 ? "-" : "+";
+
+  //trocar a classe do CSS
+  const CSSClass = amount < 0 ? "minus" : "plus";
+
+  //retirar o operador
+  const amountWithoutOperator = Math.abs(amount);
+
+  const li = document.createElement("li");
+
+  li.classList.add(CSSClass);
+  li.innerHTML = `
+    ${name} <span>${operator} R$ ${amountWithoutOperator}</span>
+    <button class='delete-btn' onClick="removeTransaction(${id})">x</buttton>
+    `;
+  transactionUl.prepend(li);
+};
+
+const getExpense = (transactionsAmounts) =>
+  Math.abs(
+    transactionsAmounts
+      .filter((value) => value < 0)
+      .reduce((acc, value) => acc + value, 0)
+      .toFixed(2)
+  );
+
+const getIncome = (transactionsAmounts) =>
+  transactionsAmounts
+    .filter((value) => value > 0)
+    .reduce((acc, transaction) => acc + transaction, 0)
+    .toFixed(2);
+
+const getTotal = (transactionsAmounts) =>
+  transactionsAmounts
+    .reduce((acc, transaction) => acc + transaction, 0)
+    .toFixed(2);
+
+const updateBalanceValues = () => {
+  const transactionsAmounts = transactions.map(({ amount }) => amount);
+
+  const total = getTotal(transactionsAmounts);
+  const income = getIncome(transactionsAmounts);
+  const expense = getExpense(transactionsAmounts);
+
+  balanceDisplay.textContent = `R$ ${total}`;
+  incomeDisplay.textContent = `R$ ${income}`;
+  expenseDisplay.textContent = `R$ ${expense}`;
+};
+
+const init = () => {
+  transactionUl.innerHTML = "";
+  transactions.forEach(addTransactionsIntoDOM);
+  updateBalanceValues();
+};
+
+init();
+
+const updateLocalStorage = () => {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+};
+
+const generateID = () => Math.round(Math.random() * 1000);
+
+const addTransactionsArray = (transactionName, transactionAmount) => {
+  transactions.push({
+    id: generateID(),
+    name: transactionName,
+    amount: Number(transactionAmount),
+  });
+};
+
+const clearInputs = () => {
+  inputTransactionName.value = "";
+  inputTransactionAmount.value = "";
+};
+
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+
+  const transactionName = inputTransactionName.value.trim();
+  const transactionAmount = inputTransactionAmount.value.trim();
+
+  const isSomeInputEmpty = transactionName === "" || transactionAmount === "";
+  if (isSomeInputEmpty) {
+    alert("por favor, preencha nome e valor da transação");
+    return;
+  }
+
+  addTransactionsArray(transactionName, transactionAmount);
+  init();
+  updateLocalStorage();
+  clearInputs();
+};
+
+form.addEventListener("submit", handleFormSubmit);
